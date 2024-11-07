c1678867d6247ab8162b5a2a5b053f37fe2cd001
SmukkeFake <74468445+SmukkeFake@users.noreply.github.com>
2024-11-06 19:22:29 -0500

    Add files via upload

---
+++ Practica 1.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Main Stage</title>
+</head>
+<body>
+<?php
+echo "Hola! este es mi primer scrip en PHP<BR>";
+
+//Este es un comentario
+
+#Esto tambien es un comentario
+
+/*Comentario
+Mas
+Extenso*/
+
+$vartext="Hola! Ahora estoy usando variables para guardar texto<BR>";
+echo $vartext;
+
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "El resultado de la suma $varnum1 y $varnum2 es igual a $res";
+?>
+</body>
+</html>
